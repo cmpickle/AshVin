61ad6cb5ca8bd0cfb28eaaeb48a8a6f2372ff328
Cameron Pickle <cmpickle@gmail.com>
2016-12-17 11:26:00 -0700

    started adding levels functionality

---
+++ app/src/main/java/com/pickle/ashvin/sprites/Toast.java
@@ -0,0 +1,46 @@
+/**
+ * A yummy toast
+ * 
+ * @author Lars Harmsen
+ * Copyright (c) <2014> <Lars Harmsen - Quchen>
+ */
+
+package com.pickle.ashvin.sprites;
+
+import com.pickle.ashvin.Game;
+import com.pickle.ashvin.GameView;
+import com.pickle.ashvin.R;
+import com.pickle.ashvin.Util;
+
+import android.graphics.Bitmap;
+
+public class Toast extends PowerUp {
+    
+    /**
+     * Static bitmap to reduce memory usage.
+     */
+    public static Bitmap globalBitmap;
+    
+    public static final int POINTS_TO_TOAST = 42;
+
+    public Toast(GameView view, Game game) {
+        super(view, game);
+        if(globalBitmap == null){
+            globalBitmap = Util.getScaledBitmapAlpha8(game, R.drawable.toast);
+        }
+        this.bitmap = globalBitmap;
+        this.width = this.bitmap.getWidth();
+        this.height = this.bitmap.getHeight();
+    }
+
+    /**
+     * When eaten the player will turn into nyan cat.
+     */
+    @Override
+    public void onCollision() {
+        super.onCollision();
+        view.changeToNyanCat();
+    }
+    
+    
+}
